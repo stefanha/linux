efe1087235e92f70485c28107f7ec567042c3b9f
Nicholas Bellinger <nab@hera.kernel.org>
2008-05-21 23:24:56 +0000

    Removed internal strstr() function for LINUX_KERNEL_22

--- drivers/lio-core/iscsi_parameters.c
+++ drivers/lio-core/iscsi_parameters.c
@@ -49,26 +49,6 @@
 
 extern iscsi_global_t *iscsi_global;
 
-#ifdef LINUX_KERNEL_22
-extern char * strstr(const char * s1,const char * s2)
-{
-	int l1, l2;
-
-	l2 = strlen(s2);
-	if (!l2)
-		return (char *) s1;
-
-	l1 = strlen(s1);
-	while (l1 >= l2) {
-		l1--;
-		if (!memcmp(s1,s2,l2))
-			return (char *) s1;
-		s1++;
-	}
-	return NULL;
-}
-#endif
-
 /*	iscsi_login_rx_data():
  *
  *
