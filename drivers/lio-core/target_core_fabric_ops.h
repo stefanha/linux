48a6ebf65d1595e2599ec088e96c6f1d5bc203f2
Nicholas Bellinger <nab@linux-iscsi.org>
2008-12-09 18:16:12 -0800

    [Target_Core_Mod]: Update struct target_core_fabric_ops
    This patch updates struct target_core_fabric_ops, and adds the following
    new function pointers:
    
    write_pending()
    get_task_tag()
    get_cmd_state()
    new_cmd_failure()
    queue_data_in()
    queue_status()
    queue_tm_rsp()
    is_state_remove()
    pack_lun()
    
    Also, all uses of iscsi_cmd_t now become se_cmd_t.
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- drivers/lio-core/target_core_fabric_ops.h
+++ drivers/lio-core/target_core_fabric_ops.h
@@ -1,12 +1,11 @@
 struct target_core_fabric_ops {
-        void (*release_cmd_to_pool)(struct iscsi_cmd_s *, struct iscsi_session_s *);
-        void (*release_cmd_direct)(struct iscsi_cmd_s *);
+        void (*release_cmd_to_pool)(struct se_cmd_s *);
+        void (*release_cmd_direct)(struct se_cmd_s *);
 	int (*dev_del_lun)(struct iscsi_portal_group_s *, __u32);
 	int (*stop_session)(struct iscsi_session_s *, int, int);
 	void (*fall_back_to_erl0)(struct iscsi_conn_s *);
-	void (*add_cmd_to_response_queue)(struct iscsi_cmd_s *, struct iscsi_conn_s *, u8);
-	int (*build_r2ts_for_cmd)(struct iscsi_cmd_s *, struct iscsi_conn_s *, int);
-	void (*dec_nacl_count)(struct iscsi_node_acl_s *, struct iscsi_cmd_s *);
+	int (*write_pending)(struct se_cmd_s *);
+	void (*dec_nacl_count)(struct iscsi_node_acl_s *, struct se_cmd_s *);
 	void *(*scsi_auth_intr_seq_start)(struct seq_file *, loff_t *);
 	void *(*scsi_auth_intr_seq_next)(struct seq_file *, void *, loff_t *);
 	int (*scsi_auth_intr_seq_show)(struct seq_file *, void *);
@@ -15,4 +14,12 @@ struct target_core_fabric_ops {
 	void *(*scsi_att_intr_port_seq_next)(struct seq_file *, void *, loff_t *);
 	int (*scsi_att_intr_port_seq_show)(struct seq_file *, void *);
 	void (*scsi_att_intr_port_seq_stop)(struct seq_file *, void *);
+	u32 (*get_task_tag)(struct se_cmd_s *);
+	int (*get_cmd_state)(struct se_cmd_s *);
+	void (*new_cmd_failure)(struct se_cmd_s *);
+	int (*queue_data_in)(struct se_cmd_s *);
+	int (*queue_status)(struct se_cmd_s *);
+	int (*queue_tm_rsp)(struct se_cmd_s *);
+	int (*is_state_remove)(struct se_cmd_s *);
+	u64 (*pack_lun)(unsigned int);
 };
