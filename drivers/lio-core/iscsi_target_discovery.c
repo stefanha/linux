192e42eed03d9a61c9cce70474f951f071f2be90
Nicholas Bellinger <nab@linux-iscsi.org>
2009-09-07 18:54:53 -0700

    [LIO-Target]: Checkpath fixes for iscsi_target_discovery.[c,h]
    This patch contains checkpatch cleanups for LIO-Target in-band discovery code, here is the output:
    
    iscsi_target_discovery.c:160: WARNING: line over 80 characters
    iscsi_target_discovery.c:162: WARNING: line over 80 characters
    total: 0 errors, 2 warnings, 185 lines checked
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- drivers/lio-core/iscsi_target_discovery.c
+++ drivers/lio-core/iscsi_target_discovery.c
@@ -1,10 +1,10 @@
-/*********************************************************************************
+/*******************************************************************************
  * Filename:  iscsi_target_discovery.c
  *
  * This file contains iSCSI Target discovery specific functions.
- * 
+ *
  * Copyright (c) 2002, 2003, 2004, 2005 PyX Technologies, Inc.
- * Copyright (c) 2005, 2006, 2007 SBE, Inc. 
+ * Copyright (c) 2005, 2006, 2007 SBE, Inc.
  * Copyright (c) 2007 Rising Tide Software, Inc.
  *
  * Nicholas A. Bellinger <nab@kernel.org>
@@ -23,7 +23,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- *********************************************************************************/
+ ******************************************************************************/
 
 
 #define ISCSI_TARGET_DISCOVERY_C
@@ -44,14 +44,11 @@
 
 #undef ISCSI_TARGET_DISCOVERY_C
 
-extern iscsi_global_t *iscsi_global;
-extern void iscsi_ntoa2 (unsigned char *, __u32);
-
 /*	iscsi_build_sendtargets_response():
  *
  *
  */
-extern int iscsi_build_sendtargets_response (iscsi_cmd_t *cmd)
+extern int iscsi_build_sendtargets_response(iscsi_cmd_t *cmd)
 {
 	char *ip, *ip_ex, *payload = NULL;
 	iscsi_conn_t *conn = CONN(cmd);
@@ -63,18 +60,16 @@ extern int iscsi_build_sendtargets_response (iscsi_cmd_t *cmd)
 	unsigned char buf[256];
 	unsigned char buf_ipv4[IPV4_BUF_SIZE];
 
-	TRACE_ENTER
-
 	buffer_len = (CONN_OPS(conn)->MaxRecvDataSegmentLength > 32768) ?
 			32768 : CONN_OPS(conn)->MaxRecvDataSegmentLength;
 
-	if (!(payload = (char *) kmalloc(buffer_len, GFP_KERNEL))) {
+	payload = kzalloc(buffer_len, GFP_KERNEL);
+	if (!(payload)) {
 		TRACE_ERROR("Unable to allocate memory for sendtargets"
 			" response.\n");
-		return(-1);
+		return -1;
 	}
-	memset((void *)payload, 0, buffer_len);
-	
+
 	spin_lock(&iscsi_global->tiqn_lock);
 	list_for_each_entry(tiqn, &iscsi_global->g_tiqn_list, tiqn_list) {
 		memset((void *)buf, 0, 256);
@@ -102,21 +97,28 @@ extern int iscsi_build_sendtargets_response (iscsi_cmd_t *cmd)
 			spin_unlock(&tpg->tpg_state_lock);
 
 			spin_lock(&tpg->tpg_np_lock);
-			list_for_each_entry(tpg_np, &tpg->tpg_gnp_list, tpg_np_list) {
+			list_for_each_entry(tpg_np, &tpg->tpg_gnp_list,
+					tpg_np_list) {
 				memset((void *)buf, 0, 256);
 
 				if (tpg_np->tpg_np->np_flags & NPF_NET_IPV6)
 					ip = &tpg_np->tpg_np->np_ipv6[0];
 				else {
 					memset(buf_ipv4, 0, IPV4_BUF_SIZE);
-					iscsi_ntoa2(buf_ipv4, tpg_np->tpg_np->np_ipv4);
+					iscsi_ntoa2(buf_ipv4,
+						tpg_np->tpg_np->np_ipv4);
 					ip = &buf_ipv4[0];
 				}
 
-				len = sprintf(buf, "TargetAddress=%s%s%s:%hu,%hu\n",
-					(tpg_np->tpg_np->np_flags & NPF_NET_IPV6) ?
-					"[" : "", ip, (tpg_np->tpg_np->np_flags & NPF_NET_IPV6) ?
-					"]" : "", tpg_np->tpg_np->np_port, tpg->tpgt);
+				len = sprintf(buf, "TargetAddress="
+					"%s%s%s:%hu,%hu\n",
+					(tpg_np->tpg_np->np_flags &
+						NPF_NET_IPV6) ?
+					"[" : "", ip,
+					(tpg_np->tpg_np->np_flags &
+						NPF_NET_IPV6) ?
+					"]" : "", tpg_np->tpg_np->np_port,
+					tpg->tpgt);
 				len += 1;
 
 				if ((len + payload_len) > buffer_len) {
@@ -130,18 +132,28 @@ extern int iscsi_build_sendtargets_response (iscsi_cmd_t *cmd)
 				payload_len += len;
 
 				spin_lock(&tpg_np->tpg_np->np_ex_lock);
-				list_for_each_entry(np_ex, &tpg_np->tpg_np->np_nex_list, np_ex_list) {
-					if (tpg_np->tpg_np->np_flags & NPF_NET_IPV6)
+				list_for_each_entry(np_ex,
+						&tpg_np->tpg_np->np_nex_list,
+						np_ex_list) {
+					if (tpg_np->tpg_np->np_flags &
+							NPF_NET_IPV6)
 						ip_ex = &np_ex->np_ex_ipv6[0];
 					else {
-						memset(buf_ipv4, 0, IPV4_BUF_SIZE);
-						iscsi_ntoa2(buf_ipv4, np_ex->np_ex_ipv4);
+						memset(buf_ipv4, 0,
+							IPV4_BUF_SIZE);
+						iscsi_ntoa2(buf_ipv4,
+							np_ex->np_ex_ipv4);
 						ip_ex = &buf_ipv4[0];
 					}
-					len = sprintf(buf, "TargetAddress=%s%s%s:%hu,%hu",
-						(tpg_np->tpg_np->np_flags & NPF_NET_IPV6) ?
-						"[" : "", ip_ex, (tpg_np->tpg_np->np_flags & NPF_NET_IPV6) ?
-						"]" : "", np_ex->np_ex_port, tpg->tpgt);
+					len = sprintf(buf, "TargetAddress="
+							"%s%s%s:%hu,%hu",
+						(tpg_np->tpg_np->np_flags &
+							NPF_NET_IPV6) ?
+						"[" : "", ip_ex,
+						(tpg_np->tpg_np->np_flags &
+							NPF_NET_IPV6) ?
+						"]" : "", np_ex->np_ex_port,
+						tpg->tpgt);
 					len += 1;
 
 					if ((len + payload_len) > buffer_len) {
@@ -151,8 +163,9 @@ extern int iscsi_build_sendtargets_response (iscsi_cmd_t *cmd)
 						end_of_buf = 1;
 						goto eob;
 					}
-					
-					memcpy((void *)payload + payload_len, buf, len);
+
+					memcpy((void *)payload + payload_len,
+							buf, len);
 					payload_len += len;
 				}
 				spin_unlock(&tpg_np->tpg_np->np_ex_lock);
@@ -168,6 +181,5 @@ eob:
 
 	cmd->buf_ptr = payload;
 
-	TRACE_LEAVE
-	return(payload_len);
+	return payload_len;
 }
