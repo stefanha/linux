5d5cc5a3e30e41daa388ea9ef7a401d5c7778476
Nicholas Bellinger <nab@linux-iscsi.org>
2009-09-08 20:45:41 -0700

    [LIO-Target]: Checkpatch and legacy cleanup for iscsi_linux_defs.h
    This patch makes iscsi_linux_defs.h checkpatch clean, and also removes some
    legacy/unused wrappers from the days of v2.x.
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- drivers/lio-core/iscsi_linux_defs.h
+++ drivers/lio-core/iscsi_linux_defs.h
@@ -1,25 +1,3 @@
-/*********************************************************************************
- * Filename:  iscsi_linux_defs.h
- *
- * This file contains wrapper definies related to LINUX functions.
- *
- * $HeadURL: svn://subversion.sbei.com/pyx/target/branches/2.9-linux-iscsi.org/pyx-target/include/iscsi_linux_defs.h $
- *   $LastChangedRevision: 7131 $
- *   $LastChangedBy: nab $
- *   $LastChangedDate: 2007-08-25 17:03:55 -0700 (Sat, 25 Aug 2007) $
- *
- * Copyright (c) 2003 PyX Technologies, Inc.
- * Copyright (c) 2006-2007 SBE, Inc.  All Rights Reserved.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * For further information, contact via email: support@sbei.com
- * SBE, Inc.  San Ramon, California  U.S.A.
- *********************************************************************************/
-
-
 #ifndef ISCSI_LINUX_DEFS_H
 #define ISCSI_LINUX_DEFS_H
 
@@ -34,39 +12,27 @@
 #include <linux/highmem.h>
 
 /*
- * Userspace access.
- */
-#define CALL_USERMODEHELPER(a, b, c)   call_usermodehelper(a, b, c, 1)
-
-
-/*
- * kernel -- userspace copy commands
- */
-#define COPY_FROM_USER(dest, src, len)		\
-	copy_from_user((void *)(dest), (void *)(src), (len))
-#define COPY_TO_USER(dest, src, len)		\
-	copy_to_user((void *)(dest), (void *)(src), (len))
-
-/*
  * Threads.
  */
-#define iscsi_daemon(thread, name, sigs) \
-	daemonize(name); \
-	current->policy = SCHED_NORMAL; \
-	set_user_nice(current, -20); \
-	spin_lock_irq(&current->sighand->siglock); \
-	siginitsetinv(&current->blocked, (sigs)); \
-	recalc_sigpending(); \
-	(thread) = current; \
-	spin_unlock_irq(&current->sighand->siglock);
+#define iscsi_daemon(thread, name, sigs)		\
+do {							\
+	daemonize(name);				\
+	current->policy = SCHED_NORMAL;			\
+	set_user_nice(current, -20);			\
+	spin_lock_irq(&current->sighand->siglock);	\
+	siginitsetinv(&current->blocked, (sigs));	\
+	recalc_sigpending();				\
+	(thread) = current;				\
+	spin_unlock_irq(&current->sighand->siglock);	\
+} while (0);
 
 /*
  * Timers and Time
  */
-#define MOD_TIMER(timer, expires)	mod_timer(timer, (get_jiffies_64() + expires * HZ))
-#define SETUP_TIMER(timer, t, d, func) \
-	timer.expires	= (get_jiffies_64() + t * HZ); \
-	timer.data	= (unsigned long) d; \
+#define MOD_TIMER(t, exp) mod_timer(t, (get_jiffies_64() + exp * HZ))
+#define SETUP_TIMER(timer, t, d, func)			\
+	timer.expires	= (get_jiffies_64() + t * HZ);	\
+	timer.data	= (unsigned long) d;		\
 	timer.function	= func;
 
 #endif    /*** ISCSI_LINUX_DEFS_H ***/
