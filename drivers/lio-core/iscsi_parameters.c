94de333b259458724f117d6d271e2519008b10f2
Nicholas Bellinger <nab@linux-iscsi.org>
2009-11-14 14:40:15 -0800

    [LIO-Target]: Make MaxConnections and ImmediateData response optional during iSCSI Login
    This patch updates iscsi_check_proposer_for_optional_reply() to setup the MaxConnections
    and ImmediateData parameter keys that are exchanged during iSCSI Login to NOT require
    a response by default.
    
    This patch is a bugfix to allow gPXE iSCSI clients to login and access LIO-Target fabrics.
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- drivers/lio-core/iscsi_parameters.c
+++ drivers/lio-core/iscsi_parameters.c
@@ -883,6 +883,11 @@ static void iscsi_check_proposer_for_optional_reply(iscsi_param_t *param)
 	} else if (IS_TYPE_BOOL_OR(param)) {
 		if (!strcmp(param->value, YES))
 			SET_PSTATE_REPLY_OPTIONAL(param);
+		 /*
+		  * Required for gPXE iSCSI boot client
+		  */
+		if (!strcmp(param->name, IMMEDIATEDATA))
+			SET_PSTATE_REPLY_OPTIONAL(param);
 	} else if (IS_TYPE_NUMBER(param)) {
 		if (!strcmp(param->name, MAXRECVDATASEGMENTLENGTH))
 			SET_PSTATE_REPLY_OPTIONAL(param);
@@ -901,6 +906,11 @@ static void iscsi_check_proposer_for_optional_reply(iscsi_param_t *param)
 		if (!strcmp(param->name, DEFAULTTIME2WAIT))
 			SET_PSTATE_REPLY_OPTIONAL(param);
 		if (!strcmp(param->name, DEFAULTTIME2RETAIN))
+			SET_PSTATE_REPLY_OPTIONAL(param);
+		/*
+		 * Required for gPXE iSCSI boot client
+		 */
+		if (!strcmp(param->name, MAXCONNECTIONS))
 			SET_PSTATE_REPLY_OPTIONAL(param);
 	} else if (IS_PHASE_DECLARATIVE(param))
 		SET_PSTATE_REPLY_OPTIONAL(param);
