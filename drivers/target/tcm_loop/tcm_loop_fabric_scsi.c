bd4020954ca5c6184581bc3f73ed267911e20f09
Nicholas Bellinger <nab@linux-iscsi.org>
2010-09-15 03:59:15 -0700

    tcm_loop: Fix TMR release patch OOPS
    This patch fixes an oops in tcm_loop_device_reset:transport_generic_free_cmd()
    where the call to tcm_loop_check_stop_free() was incorrectly releasing an
    associated TMR requests from within struct target_core_fabric_ops->check_stop_free()
    caller.  This needs to be done as the tcm_loop_device_reset() expects to call
    transport_generic_free_cmd() directly to release it's allocated struct tcm_loop
    descriptor, instead of from within the TFO->check_stop_free() codepath.
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- drivers/target/tcm_loop/tcm_loop_fabric_scsi.c
+++ drivers/target/tcm_loop/tcm_loop_fabric_scsi.c
@@ -187,6 +187,13 @@ int tcm_loop_new_cmd_map(struct se_cmd *se_cmd)
 void tcm_loop_check_stop_free(struct se_cmd *se_cmd)
 {
 	/*
+	 * Do not release struct se_cmd's containing a valid TMR
+	 * pointer.  These will be released directly in tcm_loop_device_reset()
+	 * with transport_generic_free_cmd().
+	 */
+	if (se_cmd->se_tmr_req)
+		return;
+	/*
 	 * Release the struct se_cmd, which will make a callback to release
 	 * struct tcm_loop_cmd * in tcm_loop_deallocate_core_cmd()
 	 */
