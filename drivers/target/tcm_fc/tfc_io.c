3ecc0daee9e95159d283599fb256b905190fa436
Nicholas Bellinger <nab@linux-iscsi.org>
2010-11-13 00:39:27 +0000

    Revert "tcm_fc: Clear frame_len before skipping to next frame"
    This reverts commit 437651c709056ce3a6c327bb0a74dad518039335.

--- drivers/target/tcm_fc/tfc_io.c
+++ drivers/target/tcm_fc/tfc_io.c
@@ -192,10 +192,8 @@ int ft_queue_data_in(struct se_cmd *se_cmd)
 		remaining -= tlen;
 
 		if (frame_len &&
-		    (skb_shinfo(fp_skb(fp))->nr_frags < FC_FRAME_SG_LEN)) {
-			frame_len = 0;
+		    (skb_shinfo(fp_skb(fp))->nr_frags < FC_FRAME_SG_LEN))
 			continue;
-		}
 		if (!remaining)
 			f_ctl |= FC_FC_END_SEQ;
 		fc_fill_fc_hdr(fp, FC_RCTL_DD_SOL_DATA, ep->did, ep->sid,
