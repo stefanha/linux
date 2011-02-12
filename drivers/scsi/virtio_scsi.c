c24491644e6d1c548fceda7cecd506fae817bafd
Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
2011-02-12 11:06:53 +0000

    virtio_scsi: Create a SCSI host and find cmd virtqueue
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>

--- drivers/scsi/virtio_scsi.c
+++ drivers/scsi/virtio_scsi.c
@@ -11,16 +11,92 @@
  *
  */
 
+#include <linux/slab.h>
 #include <linux/virtio.h>
 #include <linux/virtio_ids.h>
+#include <linux/virtio_config.h>
+#include <scsi/scsi_host.h>
 
-static int virtscsi_probe(struct virtio_device *dev)
+struct virtio_scsi {
+	/* Queue for commands and task management requests */
+	struct virtqueue *cmd_vq;
+};
+
+static void virtscsi_cmd_done(struct virtqueue *vq)
 {
-	return -EINVAL;
+	/* TODO */
 }
 
-static void virtscsi_remove(struct virtio_device *dev)
+static int virtscsi_queuecommand(struct Scsi_Host *sh, struct scsi_cmnd *sc)
 {
+	/* TODO */
+	return SCSI_MLQUEUE_HOST_BUSY;
+}
+
+static struct scsi_host_template virtscsi_host_template = {
+	.module = THIS_MODULE,
+	.name = "Virtio SCSI HBA",
+	.proc_name = "virtio_scsi",
+	.queuecommand = virtscsi_queuecommand,
+	.this_id = -1,
+
+	/* TODO correct these */
+	.can_queue = 1024,
+	.sg_tablesize = 256,
+	.max_sectors = 1024,
+	.dma_boundary = DISABLE_CLUSTERING,
+	.cmd_per_lun = 1,
+};
+
+static int __devinit virtscsi_probe(struct virtio_device *vdev)
+{
+	struct Scsi_Host *shost;
+	struct virtio_scsi *vscsi;
+	int err;
+
+	shost = scsi_host_alloc(&virtscsi_host_template, sizeof(*vscsi));
+	if (!shost)
+		return -ENOMEM;
+
+	vdev->priv = shost;
+	vscsi = shost_priv(shost);
+
+	vscsi->cmd_vq = virtio_find_single_vq(vdev, virtscsi_cmd_done, "cmd");
+	if (IS_ERR(vscsi->cmd_vq)) {
+		err = PTR_ERR(vscsi->cmd_vq);
+		goto virtio_find_single_vq_failed;
+	}
+
+	/* TODO correct these */
+	shost->max_id = 64;
+	shost->max_lun = 8;
+	shost->max_channel = 0;
+	shost->max_cmd_len = 32;
+
+	err = scsi_add_host(shost, &vdev->dev);
+	if (err)
+		goto scsi_add_host_failed;
+
+	return 0;
+
+scsi_add_host_failed:
+	vdev->config->del_vqs(vdev);
+virtio_find_single_vq_failed:
+	scsi_host_put(shost);
+	return err;
+}
+
+static void __devexit virtscsi_remove(struct virtio_device *vdev)
+{
+	struct Scsi_Host *shost = vdev->priv;
+
+	scsi_remove_host(shost);
+
+	/* Stop all the virtqueues. */
+	vdev->config->reset(vdev);
+
+	vdev->config->del_vqs(vdev);
+	scsi_host_put(shost);
 }
 
 static struct virtio_device_id id_table[] = {
@@ -38,7 +114,8 @@ static struct virtio_driver virtio_scsi_driver = {
 
 static int __init init(void)
 {
-	return register_virtio_driver(&virtio_scsi_driver);
+	int ret = register_virtio_driver(&virtio_scsi_driver);
+	return ret;
 }
 
 static void __exit fini(void)
