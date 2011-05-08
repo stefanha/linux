205ac5fff7586cfe91c7193e48da4ba52e4172c0
Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
2011-05-08 16:57:44 +0100

    initramfs: Don't mount configfs in virtio-scsi guest
    There is no need for configfs inside the virtio-scsi guest.  Configfs is
    only needed in the tcm_host guest.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- initramfs/virtio_scsi_guest_init.sh
+++ initramfs/virtio_scsi_guest_init.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 mount -t proc none /proc
 mount -t sysfs none /sys
-mount -t configfs none /sys/kernel/config
 mount -t debugfs none /sys/kernel/debug
 mount -t tmpfs none /tmp
 exec /bin/sh -i
