e598cfbd06530b158b83f4be2b8bee6c9d464573
Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
2011-05-08 16:55:12 +0100

    initramfs: Include configfs and TCM modules
    Switch to a modular kernel for the tcm_host VM.  It now includes
    configfs and target core mod kernel modules.  They are inserted on boot.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- initramfs/tcm_host_init.sh
+++ initramfs/tcm_host_init.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 mount -t proc none /proc
 mount -t sysfs none /sys
+insmod /lib/modules/$(uname -r)/kernel/fs/configfs/configfs.ko
 mount -t configfs none /sys/kernel/config
 mount -t debugfs none /sys/kernel/debug
 mount -t tmpfs none /tmp
+insmod /lib/modules/$(uname -r)/kernel/drivers/target/target_core_mod.ko
+insmod /lib/modules/$(uname -r)/kernel/drivers/target/target_core_file.ko
+insmod /lib/modules/$(uname -r)/kernel/drivers/target/target_core_iblock.ko
 insmod /lib/modules/$(uname -r)/kernel/drivers/target/tcm_vhost/tcm_vhost.ko
 mknod /dev/vhost-scsi c $(tr ':' ' ' </sys/class/misc/vhost-scsi/dev)
 cd /sys/kernel/config/target
