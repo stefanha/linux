01eb113ed1412201e6432a40a5c88f9a63f1fc39
Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
2011-05-11 09:22:27 +0100

    initramfs: use new qemu -vhost-scsi parameter
    The vhost_scsi code in QEMU has been split into two pieces: the
    virtio-scsi device emulation and the vhost_scsi host device.  The
    QEMU invocation needs to be updated.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- initramfs/tcm_host_init.sh
+++ initramfs/tcm_host_init.sh
@@ -11,5 +11,5 @@ insmod /lib/modules/$(uname -r)/kernel/drivers/target/target_core_iblock.ko
 insmod /lib/modules/$(uname -r)/kernel/drivers/target/tcm_vhost/tcm_vhost.ko
 mknod /dev/vhost-scsi c $(tr ':' ' ' </sys/class/misc/vhost-scsi/dev)
 cd /sys/kernel/config/target
-/usr/bin/qemu -m 512 -nographic -kernel /boot/bzImage -initrd /boot/initramfs.gz -append console=ttyS0 -device virtio-scsi-pci,wwpn=0,tpgt=0
+/usr/bin/qemu -m 512 -nographic -kernel /boot/bzImage -initrd /boot/initramfs.gz -append console=ttyS0 -vhost-scsi id=vhost-scsi0,wwpn=0,tpgt=0 -device virtio-scsi-pci,vhost-scsi=vhost-scsi0
 exec /bin/sh -i
